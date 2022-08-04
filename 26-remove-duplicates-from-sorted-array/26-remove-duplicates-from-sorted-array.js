56b19e3ca64f3c3bde20d0ff5cbf932bd8bc9364
Skipper Harris <skipdawg3000@gmail.com>
2022-08-04 15:52:37 -0700

    Time: 277 ms (9.00%), Space: 45 MB (22.95%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+let removeDuplicates = function(nums) {
+    for(let i = 0; i < nums.length; i++) {
+        for(let j = nums.length - 1; j > i; j--) {
+            if (nums[i] === nums[j]) {
+                nums.splice(j, 1);
+            }
+        }
+    }
+};
