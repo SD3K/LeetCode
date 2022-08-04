3561959bac7bcb6123965e14d7d18c258b01527c
Skipper Harris <skipdawg3000@gmail.com>
2022-08-04 10:39:06 -0700

    Time: 124 ms (62.57%), Space: 45.7 MB (57.14%) - LeetHub

--- 39-combination-sum/39-combination-sum.js
+++ 39-combination-sum/39-combination-sum.js
@@ -10,17 +10,16 @@ function combinationSum(candidates, target) {
   const getSums = function(index, sum) {
     if (sum === target) {
       solution.push([...innerSol]);
-    } else {
-      for (let i = index; i < candidates.length; i++) {
-        let innerSum = sum + candidates[i];
-        
-        if (innerSum <= target) {
-          innerSol.push(candidates[i]);
-          
-          getSums(i, innerSum);
-          
-          innerSol.pop();
-        }
+    } 
+    for (let i = index; i < candidates.length; i++) {
+      let innerSum = sum + candidates[i];
+
+      if (innerSum <= target) {
+        innerSol.push(candidates[i]);
+
+        getSums(i, innerSum);
+
+        innerSol.pop();
       }
     }
   }
