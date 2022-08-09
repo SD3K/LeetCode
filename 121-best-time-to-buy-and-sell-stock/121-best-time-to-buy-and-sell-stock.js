dddd81b98fef59c83aa48275242212417748b717
Skipper Harris <skipdawg3000@gmail.com>
2022-08-09 10:02:31 -0700

    Time: 146 ms (26.58%), Space: 52.1 MB (16.11%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -4,12 +4,9 @@
  */
 function maxProfit(prices) {
   let result = 0;
-  let low = undefined;
+  let low = prices[0];
   let high = 0;
   for (let i = 0; i < prices.length; i++) {
-    if (low === undefined) {
-      low = prices[i];
-    }
     if (prices[i] < low) {
       low = prices[i];
       high = 0;
