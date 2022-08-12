c95e3f4f57fbb69675bd01f263b4b3aa7c5e850e
Skipper Harris <skipdawg3000@gmail.com>
2022-08-12 10:13:36 -0700

    Time: 162 ms (56.91%), Space: 49.9 MB (40.06%) - LeetHub

--- 155-min-stack/155-min-stack.js
+++ 155-min-stack/155-min-stack.js
@@ -9,7 +9,14 @@ var MinStack = function() {
  * @return {void}
  */
 MinStack.prototype.push = function(val) {
-  this.storage[this.index] = val;
+  this.storage[this.index] = { val };
+  if (this.index === 0) {
+    this.storage[this.index].min = val;
+  } else if (this.storage[this.index].val < this.storage[this.index - 1].min) {
+    this.storage[this.index].min = val;
+  } else {
+    this.storage[this.index].min = this.storage[this.index - 1].min;
+  }
   this.index += 1;
 };
 
@@ -18,7 +25,7 @@ MinStack.prototype.push = function(val) {
  */
 MinStack.prototype.pop = function() {
   if (this.index === 0) return;
-  let popped = this.storage[this.index - 1];
+  let popped = this.storage[this.index - 1].val;
   delete this.storage[this.index - 1];
   this.index -= 1;
   return popped;
@@ -29,7 +36,7 @@ MinStack.prototype.pop = function() {
  */
 MinStack.prototype.top = function() {
   if (this.index > 0) {
-    return this.storage[this.index - 1];
+    return this.storage[this.index - 1].val;
   }
 };
 
@@ -37,7 +44,7 @@ MinStack.prototype.top = function() {
  * @return {number}
  */
 MinStack.prototype.getMin = function() {
-  return Math.min(...Object.values(this.storage));
+  return this.storage[this.index - 1].min;
 };
 
 /** 
