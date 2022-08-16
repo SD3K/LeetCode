d849253afea4403bc3ee6c438db14b3f39182b79
Skipper Harris <skipdawg3000@gmail.com>
2022-08-16 09:53:23 -0700

    Time: 115 ms (86.71%), Space: 45.3 MB (63.33%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
@@ -5,7 +5,10 @@
 function firstUniqChar(s) {
   let map = new Map();
   for (let i = 0; i < s.length; i++) {
-    if (s.lastIndexOf(s[i]) === i && !map.has(s[i])) {
+    if (map.has(s[i])) {
+      continue;
+    }
+    if (s.lastIndexOf(s[i]) === i) {
       return i;
     }
     map.set(s[i])
