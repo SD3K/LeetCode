b5ad086fb40e388e885495eaa4c0b619e2d386e4
Skipper Harris <skipdawg3000@gmail.com>
2022-08-17 09:58:58 -0700

    Time: 76 ms (82.66%), Space: 42.5 MB (91.06%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.js
+++ 804-unique-morse-code-words/804-unique-morse-code-words.js
@@ -2,29 +2,25 @@
  * @param {string[]} words
  * @return {number}
  */
-function uniqueMorseRepresentations(words) {
-  const morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
-  const alpha = 'abcdefghijklmnopqrstuvwxyz';
-  let hash = {
-    count: 0
-  };
 
-  for (let i = 0; i < alpha.length; i++) {
-    if (hash[alpha[i]] === undefined) {
-      hash[alpha[i]] = morse[i];
-    }
+const morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
+const alpha = 'abcdefghijklmnopqrstuvwxyz';
+let hash = {};
+for (let i = 0; i < alpha.length; i++) {
+  if (hash[alpha[i]] === undefined) {
+    hash[alpha[i]] = morse[i];
   }
-  
+}
+
+function uniqueMorseRepresentations(words) {
+  let map = new Map();
   for (let i = 0; i < words.length; i++) {
     let morseWord = '';
     for (let j = 0; j < words[i].length; j++) {
       morseWord += hash[words[i][j]];
     }
-    if (hash[morseWord] === undefined) {
-      hash[morseWord] = true;
-      hash.count++
-    }
+    map.set(morseWord);
   }
   
-  return hash.count;
+  return map.size;
 };
