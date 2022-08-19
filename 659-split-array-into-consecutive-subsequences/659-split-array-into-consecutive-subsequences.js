457caabb3f9ae5e86b9c82a97e0f3bedaa64eed9
Skipper Harris <skipdawg3000@gmail.com>
2022-08-19 10:31:53 -0700

    Time: 152 ms (51.25%), Space: 51.4 MB (27.50%) - LeetHub

---
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isPossible = function(nums) {
+    if(nums.length < 3) return false;
+    
+    const freqMap = new Map(), appendfreq = new Map();
+    nums.forEach(num => {
+        if (!freqMap.has(num))
+            freqMap.set(num, 0);
+        
+        freqMap.set(num, freqMap.get(num) + 1);
+    });
+    
+     for (let i of nums) {
+        if (freqMap.get(i) == 0) continue;
+        else if ((appendfreq.get(i) || 0) > 0) {
+            appendfreq.set(i, appendfreq.get(i) - 1);
+            appendfreq.set(i+1, (appendfreq.get(i+1) || 0) + 1);
+        }   
+        else if ((freqMap.get(i+1) || 0) > 0 && (freqMap.get(i+2) || 0) > 0) {
+            freqMap.set(i+1, freqMap.get(i+1) - 1);
+            freqMap.set(i+2, freqMap.get(i+2) - 1);
+            appendfreq.set(i+3, (appendfreq.get(i+3) || 0) + 1);
+        }
+        else return false;
+        freqMap.set(i, freqMap.get(i) - 1);
+    }
+    return true;
+};
