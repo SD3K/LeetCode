8607e1670f4e94f327f815c8c3eb09fee4be86f5
Skipper Harris <skipdawg3000@gmail.com>
2022-08-24 09:56:11 -0700

    Time: 410 ms (23.29%), Space: 51.3 MB (41.21%) - LeetHub

--- 326-power-of-three/326-power-of-three.js
+++ 326-power-of-three/326-power-of-three.js
@@ -3,9 +3,5 @@
  * @return {boolean}
  */
 function isPowerOfThree(n) {
-  if (n === 1) return true;
-  while(n > 3) {
-    n /= 3;
-  }
-  return n === 3 ? true : false;
+  return n > 0 && 1162261467 % n === 0
 };
