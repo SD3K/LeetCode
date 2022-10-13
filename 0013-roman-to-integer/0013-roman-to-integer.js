d844627b8ff5bff2d066aa166ed6bf8942e27a96
Skipper Harris <skipdawg3000@gmail.com>
2022-10-13 14:30:29 -0700

    Time: 265 ms (17.48%), Space: 47.7 MB (41.89%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const roman = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000
+}
+
+function romanToInt(s) {
+  let num = 0;
+  
+  for (let i = 0; i < s.length; i++) {
+    if (s[i + 1] !== undefined && roman[s[i]] < roman[s[i + 1]]) {
+      num += roman[s[i + 1]] - roman[s[i]];
+      i += 1;
+    } else {
+      num += roman[s[i]];
+    }
+  }
+
+  return num;
+};
