d865054befe5cef42acb426c46a64bfef73877fc
Skipper Harris <skipdawg3000@gmail.com>
2022-10-13 15:24:34 -0700

    Time: 108 ms (17.21%), Space: 42.3 MB (30.27%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+const t9 = {
+  2: 'abc',
+  3: 'def',
+  4: 'ghi',
+  5: 'jkl',
+  6: 'mno',
+  7: 'pqrs',
+  8: 'tuv',
+  9: 'wxyz'
+}
+
+function letterCombinations(digits) {
+  let res = [];
+  let length = digits.length;
+  
+  if (!length) {
+    return res;
+  }
+  
+  function buildWords(index = 0, word = '') {
+    if(index === length) {
+      res.push(word);
+    } else {
+      let letters = t9[digits[index]];
+      for (let i = 0; i < letters.length; i++) {
+        buildWords(index + 1, word + letters[i]);
+      }
+    }
+  }
+  buildWords();
+  return res;
+};
