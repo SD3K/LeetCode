172ecf998ecbb4b0eeb68c7ee767471b627c7ca7
Skipper Harris <skipdawg3000@gmail.com>
2022-10-19 10:49:48 -0700

    Time: 126 ms (53.65%), Space: 44.7 MB (87.17%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.js
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.js
@@ -5,22 +5,17 @@
  */
 function topKFrequent(words, k) {
   const map = {};
-  console.log(words);
   words.sort();
-  console.log(words);
   
   for (let i = 0; i < words.length; i++) {
     map[words[i]] === undefined ? map[words[i]] = 1 : map[words[i]] += 1;
   }
-  console.log(map);
   
   const dedupe = Object.keys(map);
-  console.log(dedupe);
   
   dedupe.sort((a, b) => {
     return map[b] - map[a];
   });
-  console.log(dedupe);
   
   return dedupe.slice(0, k);
 };
