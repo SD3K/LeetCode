e56d4cab4ac18b085bcb6d42428d32b02a38fecb
Skipper Harris <skipdawg3000@gmail.com>
2022-10-21 13:36:55 -0700

    Time: 180 ms (58.29%), Space: 55.7 MB (80.34%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -4,12 +4,19 @@
  * @return {boolean}
  */
 function containsNearbyDuplicate(nums, k) {
+  let set = new Set();
+  
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j <= i + k && j < nums.length; j++) {
-      if (nums[i] === nums[j] && Math.abs(i - j) <= k) {
-        return true;
-      }
+    if (set.has(nums[i])) {
+      return true;
+    }
+    
+    set.add(nums[i]);
+    
+    if (set.size >= k) {
+      set.delete(nums[i - k]);
     }
   }
+  
   return false;
 };
